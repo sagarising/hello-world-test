0b5b5d8033616bf8b7486f9c8769ff8cf7af3c94
Sagar Maurya <sagarising@yahoo.com>
2021-04-11 15:35:33 +0530

    add cypress code to tweet on twitter

---
+++ README.md
@@ -0,0 +1,11 @@
+## Overview
+
+Tweets on twitter through cypress.
+
+##Pre-requisites
+* node
+* npm
+* ENV variable that has your twitter password. On bash you can do that by `export CYPRESS_TWITTER_PASSWORD='*******'` before running cypress.
+
+To run/view tests:
+`npx cypress open`
---
+++ cypress/support/index.js
@@ -0,0 +1,24 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  //XHR reuest were failing on twitter for fetching some video content. I didn't needed it anyway.
+  return false
+})
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
